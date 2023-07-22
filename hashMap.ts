aef17c38a5a69c8bc20f3c0572db8afa1fe439fa
rutuser <micharuzicka.r@gmail.com>
2023-07-22 18:49:21 +0100

    hashmap

---
+++ hashMap.ts
@@ -0,0 +1,69 @@
+type K = string | number | boolean;
+
+class MyHashMap<T> {
+	nItems: number = 0;
+	table: Array<[[K, T]]> = new Array<[[K, T]]>(100);
+
+	private calculateHash(key: K): number {
+		let hash = 17;
+		const keyString = key.toString();
+
+		for (let i = 0; i < keyString.length; i++) {
+			hash = (hash * keyString.charCodeAt(i)) % this.table.length;
+		}
+
+		return hash;
+	}
+
+	private resize(): void {
+		const resizedTable = new Array<[[K, T]]>(this.table.length * 2);
+
+		for (let i = 0; i < this.table.length; i++) {
+			if (!this.table[i]) break;
+
+			for (let j = 0; j < this.table[i].length; j++) {
+				const [key, value] = this.table[i][j];
+				const idx = this.calculateHash(key);
+
+				resizedTable[idx].push([key, value]);
+			}
+		}
+
+		this.table = resizedTable;
+	}
+
+	private getLoadFactor(): number {
+		return this.nItems / this.table.length;
+	}
+
+	public get(key: K): T | undefined {
+		const idx = this.calculateHash(key);
+		return this.table[idx].find(([_, k]) => k === key)?.[1];
+	}
+
+	public set(key: K, value: T): void {
+		this.nItems++;
+		const loadFactor = this.getLoadFactor();
+
+		if (loadFactor > 0.8) this.resize();
+
+		const idx = this.calculateHash(key);
+		this.table[idx].push([key, value]);
+	}
+}
+
+const hashTable = new MyHashMap<number>();
+
+hashTable.set("firstName", 0);
+
+hashTable.set("lastName", 1);
+hashTable.set("age", 2);
+hashTable.set("bob", 3);
+console.log(hashTable.get("firstName"));
+console.log(hashTable.get("lastName"));
+console.log(hashTable.get("age"));
+console.log(hashTable.get("bob"));
+
+{
+	firstName: 0;
+}
